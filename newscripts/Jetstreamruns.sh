1f5456b0d37a7e4c8bd7077492c27f50c73f01bc
Thomas McCarthy <mccarthy.tw@gmail.com>
2017-03-15 17:16:43 -0400

    updated Jetstreamruns.sh because the while loop wasn't working

--- newscripts/Jetstreamruns.sh
+++ newscripts/Jetstreamruns.sh
@@ -6,15 +6,17 @@ queries=/vol1/*nt_ncRNAs/Chr*.fa
 genomelibraries=/vol1/3KRiceReads/data/*/*.library
 
 index=0
-while [ $index -lt 10 ]
+for query in $queries
 do
-    for query in $queries
-    do
-        (( index += 1 ))
+    if [ $index -lt 10 ]; then
+        #echo "index is $index, query is $query"
         for library in $genomelibraries
-        do
-            ( sh SRAssembler_run_generator.sh $query $library cdna "23" ) >> "$(date +%F)"_run.sh
-        done
-    done
+            do
+                ( sh SRAssembler_run_generator.sh $query $library cdna "23" ) >> "$(date +%F)"_run.sh
+            done
+        (( index += 1 ))
+    else
+        break
+    fi
 done
 
