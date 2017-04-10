fae38494656045cb0a492a9314a103fecc3e9b74
Thomas Wayne McCarthy <mccarthy.tw@gmail.com>
2017-04-10 11:26:11 -0400

    update Masonruns.sh to generate PBS scripts for each query separately

--- newscripts/Masonruns.sh
+++ newscripts/Masonruns.sh
@@ -2,30 +2,19 @@
 set -euo pipefail
 
 # These must be absolute paths
-queries=/N/dc2/scratch/twmccart/Mason/OSprobes/XP_015620933.1.fa
+queries=/N/dc2/scratch/twmccart/Mason/OSprobes/*.fa
 genomelibraries=/N/dc2/scratch/twmccart/3KRiceReads/data/*/*.library
 
-echo "#!/bin/bash" >> "$(date +%F)"_Masonrun.pbs
-echo "#PBS -k o" >> "$(date +%F)"_Masonrun.pbs
-echo "#PBS -l nodes=1:ppn=24,walltime=10:00:00,vmem=60gb" >> "$(date +%F)"_Masonrun.pbs
-echo "#PBS -M twmccart@iu.edu" >> "$(date +%F)"_Masonrun.pbs
-echo "#PBS -m abe" >> "$(date +%F)"_Masonrun.pbs
-echo "#PBS -N JobName" >> "$(date +%F)"_Masonrun.pbs
-echo "#PBS -j oe" >> "$(date +%F)"_Masonrun.pbs
-
-index=0
-for query in $queries
-do
-    #if [ $index -lt 10 ]; then
-    if true; then
-        #echo "index is $index, query is $query"
-        for library in $genomelibraries
-            do
-                ( sh SRAssembler_run_generator.sh $query $library protein "24" ) >> "$(date +%F)"_Masonrun.pbs
-            done
-        (( index += 1 ))
-    else
-        break
-    fi
+for query in $queries; do
+    echo "#!/bin/bash" > ${query}_Masonrun.pbs
+    echo "#PBS -k o" >> ${query}_Masonrun.pbs
+    echo "#PBS -l nodes=1:ppn=24,walltime=10:00:00,vmem=60gb" >> ${query}_Masonrun.pbs
+    echo "#PBS -M twmccart@iu.edu" >> ${query}_Masonrun.pbs
+    echo "#PBS -m abe" >> ${query}_Masonrun.pbs
+    echo "#PBS -N ${query}" >> ${query}_Masonrun.pbs
+    echo "#PBS -j oe" >> ${query}_Masonrun.pbs
+    for library in $genomelibraries; do
+        ( sh SRAssembler_run_generator.sh $query $library protein "24" ) >> ${query}_Masonrun.pbs
+        done
 done
 
