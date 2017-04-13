d60a4c654371cdd3a0541b2f0cdd1e8add7b964b
Thomas Wayne McCarthy <mccarthy.tw@gmail.com>
2017-04-13 13:47:29 -0400

    Now request 32 processors on one node

--- newscripts/Masonruns.sh
+++ newscripts/Masonruns.sh
@@ -5,17 +5,19 @@ set -euo pipefail
 queries=/N/dc2/scratch/twmccart/Mason/OSprobes/*.fa
 genomelibraries=/N/dc2/scratch/twmccart/3KRiceReads/data/*/*.library
 
-for query in $queries; do
-    echo "#!/bin/bash" > ${query%.fa}_Masonrun.pbs
-    echo "#PBS -k o" >> ${query%.fa}_Masonrun.pbs
-    echo "#PBS -l procs=24,walltime=24:00:00,vmem=60gb" >> ${query%.fa}_Masonrun.pbs
-    echo "#PBS -M twmccart@iu.edu" >> ${query%.fa}_Masonrun.pbs
-    echo "#PBS -m abe" >> ${query%.fa}_Masonrun.pbs
-    echo "#PBS -N ${query##*/}" >> ${query%.fa}_Masonrun.pbs
-    echo "#PBS -j oe" >> ${query%.fa}_Masonrun.pbs
-    for library in $genomelibraries; do
-        ( sh SRAssembler_run_generatorMason.sh $query $library protein "24" ) >> ${query%.fa}_Masonrun.pbs
-        done
-    cp ${query%.fa}_Masonrun.pbs /N/dc2/scratch/twmccart/Mason/
-done
+title="32ppn"
 
+for query in $queries; do
+    echo "#!/bin/bash" > ${query%.fa}_Masonrun.${title}.pbs
+    echo "#PBS -k o" >> ${query%.fa}_Masonrun.${title}.pbs
+    echo "#PBS -l nodes=1:ppn=32,walltime=24:00:00,vmem=60gb" >> ${query%.fa}_Masonrun.${title}.pbs
+    echo "#PBS -M twmccart@iu.edu" >> ${query%.fa}_Masonrun.${title}.pbs
+    echo "#PBS -m abe" >> ${query%.fa}_Masonrun.${title}.pbs
+    echo "#PBS -N ${query##*/}.${title}" >> ${query%.fa}_Masonrun.${title}.pbs
+    echo "#PBS -j oe" >> ${query%.fa}_Masonrun.${title}.pbs
+    for library in $genomelibraries; do
+        ( sh ~/SRAssembler/newscripts/SRAssembler_run_generatorMason.sh $query $library protein "32" ) >>
+ ${query%.fa}_Masonrun.${title}.pbs
+        done
+    cp ${query%.fa}_Masonrun.${title}.pbs /N/dc2/scratch/twmccart/Mason/
+done
