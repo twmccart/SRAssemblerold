558ba7ca06cd23e0949fa8329bb0df500e26f7e1
Thomas Wayne McCarthy <mccarthy.tw@gmail.com>
2017-04-13 11:16:11 -0400

    Updated Masonruns.sh to ask for processors instead of nodes, changed where it saves the pbs files for submission to scratch

--- newscripts/Masonruns.sh
+++ newscripts/Masonruns.sh
@@ -8,7 +8,7 @@ genomelibraries=/N/dc2/scratch/twmccart/3KRiceReads/data/*/*.library
 for query in $queries; do
     echo "#!/bin/bash" > ${query%.fa}_Masonrun.pbs
     echo "#PBS -k o" >> ${query%.fa}_Masonrun.pbs
-    echo "#PBS -l nodes=4:ppn=6,walltime=5:00:00,vmem=60gb" >> ${query%.fa}_Masonrun.pbs
+    echo "#PBS -l procs=24,walltime=24:00:00,vmem=60gb" >> ${query%.fa}_Masonrun.pbs
     echo "#PBS -M twmccart@iu.edu" >> ${query%.fa}_Masonrun.pbs
     echo "#PBS -m abe" >> ${query%.fa}_Masonrun.pbs
     echo "#PBS -N ${query##*/}" >> ${query%.fa}_Masonrun.pbs
@@ -16,6 +16,6 @@ for query in $queries; do
     for library in $genomelibraries; do
         ( sh SRAssembler_run_generatorMason.sh $query $library protein "24" ) >> ${query%.fa}_Masonrun.pbs
         done
-    cp ${query%.fa}_Masonrun.pbs ~/
+    cp ${query%.fa}_Masonrun.pbs /N/dc2/scratch/twmccart/Mason/
 done
 
