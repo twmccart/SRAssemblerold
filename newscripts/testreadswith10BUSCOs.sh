d92e6572eb8c0389896cb0005093a176c3978481
Thomas Wayne McCarthy <mccarthy.tw@gmail.com>
2017-04-25 14:50:20 -0400

    Updated backtick notation

--- newscripts/testreadswith10BUSCOs.sh
+++ newscripts/testreadswith10BUSCOs.sh
@@ -2,11 +2,11 @@
 # Argument 1 should be the library configuration file
 
 libraryfile=$1
-BUSCOlist=`find /scratch/twmccart/SRAssembler/BUSCO/eukaryota_odb9_individualBUSCOs/ -name "*.fa" -print | shuf -n 10`
+BUSCOlist=$(find /scratch/twmccart/SRAssembler/BUSCO/eukaryota_odb9_individualBUSCOs/ -name "*.fa" -print | shuf -n 10)
 
 for BUSCO in $BUSCOlist
 do
-	command=`bash ~/Scripts/SRAssembler_run_generator.sh $BUSCO $libraryfile "protein" `
+	command=$(bash ~/Scripts/SRAssembler_run_generator.sh $BUSCO $libraryfile "protein" )
 	echo $command ";"
 	#echo ""
 	#source $command
